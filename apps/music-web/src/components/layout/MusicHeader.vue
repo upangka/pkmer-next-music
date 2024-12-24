f4d0111407956a68c362623b9bb49279bca17d23
pkmer <q10viking@outlook.com>
2024-12-24 14:42:20 +0800

    feat(music-web): :sparkles: 迁移vue项目到turborepo中

--- apps/music-web/src/components/layout/MusicHeader.vue
+++ apps/music-web/src/components/layout/MusicHeader.vue
@@ -8,25 +8,18 @@ const inputFocusStatus = ref(true)
     <!-- 左边start -->
     <section class="flex items-center justify-start gap-3">
       <iconify-icon icon="tdesign:music-rectangle-add-filled" height="36"></iconify-icon>
-      <h1 class="text-xl font-bold">Pkmer-Music</h1>
+      <h1 class="text-xl font-bold">Pkmer-Next-Music</h1>
       <nav>
         <ul class="flex items-center justify-start gap-6">
           <li>首页</li>
           <li>歌单</li>
           <li>歌手</li>
           <li class="relative">
-            <iconify-icon
-              v-show="inputFocusStatus"
-              icon="iconamoon:search-thin"
-              class="text-1xl absolute left-1 top-1/2 -translate-y-1/2 text-slate-500"
-            ></iconify-icon>
-            <input
-              @focus="inputFocusStatus = false"
-              @blur="inputFocusStatus = true"
-              type="text"
+            <iconify-icon v-show="inputFocusStatus" icon="iconamoon:search-thin"
+              class="text-1xl absolute left-1 top-1/2 -translate-y-1/2 text-slate-500"></iconify-icon>
+            <input @focus="inputFocusStatus = false" @blur="inputFocusStatus = true" type="text"
               placeholder="search something..."
-              class="w-80 rounded-md border border-gray-600 p-0.5 pl-5 focus:outline-none"
-            />
+              class="w-80 rounded-md border border-gray-600 p-0.5 pl-5 focus:outline-none" />
           </li>
         </ul>
       </nav>
--- apps/music-web/vite.config.ts
+++ apps/music-web/vite.config.ts
@@ -4,10 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   resolve: {
     alias: [
       {
