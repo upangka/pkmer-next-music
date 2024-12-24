87eca7c2134f4dc4752ff5370cefba80e9ee3b41
pkmer <q10viking@outlook.com>
2024-12-24 15:16:05 +0800

    feat(react-components): :sparkles: react组件成功打包

--- apps/music-management/src/app/page.tsx
+++ apps/music-management/src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
 import Image from 'next/image'
+import { Avatar } from '@pkmer-music-ui/react/avatar'
 
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <Avatar.Root>
+        <Avatar.Fallback>PK</Avatar.Fallback>
+        <Avatar.Image src='https://i.pravatar.cc/300' />
+      </Avatar.Root>
+
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         <Image
           className='dark:invert'
