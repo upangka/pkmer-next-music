8f0500f6866c117166058f02c6e1df5db0d8ebd0
pkmer <q10viking@outlook.com>
2024-12-28 00:36:46 +0800

    feat(asside): :sparkles: 添加动画效果

--- apps/music-web/src/components/music/PlayAssider.vue
+++ apps/music-web/src/components/music/PlayAssider.vue
@@ -1,20 +1,35 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+const showAssider = ref(true);
 </script>
 
 <template>
-  <section class="assider-container">
-    <h1 class="title">当前播放</h1>
-    <p class="total">共0首</p>
-    <ul class="songs-list gap-2">
-      <li class="playing">月亮里的阿妹 (DJ村长版|DJ版)
-      </li>
-      <li>黄昏DJ</li>
-    </ul>
-  </section>
+  <Transition name="fade">
+    <section v-if="showAssider" class="assider-container">
+      <h1 class="title">当前播放</h1>
+      <p class="total">共0首</p>
+      <ul class="songs-list gap-2">
+        <li class="playing">月亮里的阿妹 (DJ村长版|DJ版)
+        </li>
+        <li>黄昏DJ</li>
+      </ul>
+    </section>
+  </Transition>
+
 </template>
 
 <style lang="scss" scoped>
 @import "@pkmer-music/web/assets/styles/__variables.scss";
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(100%)
+}
 
 .assider-container {
   position: fixed;
@@ -25,6 +40,7 @@
   background-color: white;
   width: 300px;
   z-index: $music-assider-z-index;
+
 
   .title {
     text-align: center;
