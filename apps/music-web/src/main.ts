cf01227c6ac78e97db05a37d323bb3bb5df8d52d
pkmer <q10viking@outlook.com>
2024-12-29 00:05:50 +0800

    feat(app): :sparkles: 格式化main.ts

--- apps/music-web/src/main.ts
+++ apps/music-web/src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
-import 'iconify-icon'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'iconify-icon'
+import './assets/main.css'
 import router from '@pkmer-music/web/router'
 import App from '@pkmer-music/web/App.vue'
 import '@pkmer-music-ui/vue/theme.css'
-const app = createApp(App).use(router)
-console.log(app._context)
+
+const pinia = createPinia()
+
+const app = createApp(App).use(router).use(pinia)
 
 if ('app' in window) {
   app.mount(window['app']! as HTMLDivElement)
