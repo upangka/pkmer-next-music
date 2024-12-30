2a1d84c777c6deef894e502f3c10512768fbb2c9
pkmer <q10viking@outlook.com>
2024-12-31 00:54:13 +0800

    feat(java): :sparkles: 初始化后端项目

---
+++ apps/music-api/music-main/src/main/java/io/gitee/pkmer/MusicApplication.java
@@ -0,0 +1,28 @@
+package io.gitee.pkmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:1193094618@qq.com">pkmer</a>
+ * <br>
+ * <a href = "https://gitee.com/developeros/videos-online">Code Repository</a>
+ * At 2024/12/31
+ */
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class MusicApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MusicApplication.class);
+        app.run(args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
