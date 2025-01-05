4b27d3b36d95ae9ab8325cc09010be4ee3ede9e6
pkmer <q10viking@outlook.com>
2025-01-05 22:00:34 +0800

    feat(song_list): :sparkles: 准备开发歌单列表

--- apps/music-api/music-core/music-application/src/main/java/io/gitee/pkmer/music/application/banner/all/GetAllHandler.java
+++ apps/music-api/music-core/music-application/src/main/java/io/gitee/pkmer/music/application/banner/all/GetAllHandler.java
@@ -5,6 +5,9 @@ import io.gitee.pkmer.ddd.shared.command.CommandHandler;
 import io.gitee.pkmer.music.domain.banner.BannerEntity;
 import io.gitee.pkmer.music.domain.banner.BannerRepository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -16,8 +19,10 @@ import java.util.List;
  * <a href = "https://gitee.com/developeros/videos-online">Code Repository</a>
  * At 2025/1/5
  */
+@Slf4j
 @Component
-public class GetAllHandler implements CommandHandler<GetAllCmd, List<BannerDto>> {
+public class GetAllHandler implements CommandHandler<GetAllCmd, List<BannerDto>>, EnvironmentAware {
+    private String minioUrl;
     private final  BannerRepository bannerRepository;
 
     public GetAllHandler(BannerRepository bannerRepository){
@@ -31,10 +36,25 @@ public class GetAllHandler implements CommandHandler<GetAllCmd, List<BannerDto>>
         List<BannerDto> results = new ArrayList<>();
         for (BannerEntity banner : banners) {
             BannerDto bannerDto = new BannerDto().setId(banner.getId().value())
-                    .setPic(banner.getPic());
+                    .setPic(buildMinioUrl(banner.getPic()));
             results.add(bannerDto);
         }
         return results;
     }
 
+    private String buildMinioUrl(String objectName){
+        return minioUrl + "/" + objectName;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        String url = environment.getProperty("pkmer.minio.url");
+        if (url == null || url.isBlank()){
+            log.error("通过 pkmer.minio.url 获取 minio url为空");
+            throw new RuntimeException("minio url is null");
+        }else{
+            log.info("minio url 设置为 {}",url);
+            this.minioUrl = url;
+        }
+    }
 }
