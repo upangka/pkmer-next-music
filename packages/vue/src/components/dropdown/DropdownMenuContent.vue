c8da1a1e7707c10e6ff4318834b1aa69ccd923c5
pkmer <q10viking@outlook.com>
2025-01-24 09:56:03 +0800

    feat(dropdown): 添加变量

--- packages/vue/src/components/dropdown/DropdownMenuContent.vue
+++ packages/vue/src/components/dropdown/DropdownMenuContent.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
 import { inject } from 'vue'
+import Indicator from './Indicator.vue'
 import { dropdownMenuKey } from './constants'
 const { visible } = inject(dropdownMenuKey)!
 </script>
 <template>
-  <ul v-if="visible" class="relative w-fit rounded-md bg-black p-2 text-white">
-    <div class="indicator"></div>
+  <ul v-if="visible" class="relative w-fit rounded-md p-2 text-white">
+    <Indicator />
     <slot></slot>
   </ul>
 </template>
 <style lang="scss" scoped>
-$dropdown-background: black;
-.indicator {
-  position: absolute;
-  width: 0;
-  height: 0;
-  top: -9px;
-  left: 50%;
-  margin-left: -10px;
-  border-style: solid;
-  border-width: 0px 10px 10px 10px;
-  border-color: transparent transparent $dropdown-background transparent;
+@use './variables.scss' as dropdown;
+ul {
+  background-color: dropdown.$dropdown-background;
 }
 </style>
---
+++ packages/vue/src/components/dropdown/Indicator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="indicator"></div>
+</template>
+<style lang="scss" scoped>
+@use './variables.scss' as dropdown;
+.indicator {
+  position: absolute;
+  width: 0;
+  height: 0;
+  top: -9px;
+  left: 50%;
+  margin-left: -10px;
+  border-style: solid;
+  border-width: 0px 10px 10px 10px;
+  border-color: transparent transparent dropdown.$dropdown-background transparent;
+}
+</style>
