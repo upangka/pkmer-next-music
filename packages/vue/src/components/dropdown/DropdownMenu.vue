0c5ed7cf8e36340f2b3476ff8361ccfa33120d1d
pkmer <q10viking@outlook.com>
2025-01-24 08:08:41 +0800

    feat(dropdown): 搭建基础结构

---
+++ packages/vue/src/components/dropdown/DropdownMenu.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <details>
+    <slot></slot>
+  </details>
+</template>
---
+++ packages/vue/src/components/dropdown/DropdownMenuTrigger.vue
@@ -0,0 +1,5 @@
+<template>
+  <summary>
+    <slot />
+  </summary>
+</template>
