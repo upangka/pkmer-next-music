53367b205a4fd3ae56a15363dd870d4d6c6455b3
pkmer <q10viking@outlook.com>
2025-02-05 07:20:19 +0800

    feat(shadcn): 引入shadcn success

--- apps/music-management/src/app/dashborder/page.tsx
+++ apps/music-management/src/app/dashborder/page.tsx
@@ -1,4 +1,7 @@
 import PkmerIcon from '@pkmer-music/management/components/icon/PkmerIcon'
+import { AlertCircle } from 'lucide-react'
+import { Alert, AlertDescription, AlertTitle } from '@pkmer-music/management/components/ui/alert'
+
 export default function DashBorder() {
   return (
     <>
@@ -7,6 +10,12 @@ export default function DashBorder() {
         <PkmerIcon icon='mynaui:menu-solid' />
       </section>
       {/* header end */}
+
+      <Alert variant='destructive'>
+        <AlertCircle className='h-4 w-4' />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>Your session has expired. Please log in again.</AlertDescription>
+      </Alert>
     </>
   )
 }
