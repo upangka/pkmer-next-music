d182168d8b9e144e1ef4ffa10c7f829b48df1dbc
pkmer <q10viking@outlook.com>
2025-02-05 14:40:48 +0800

    feat(dashboarder): 修改描述

--- apps/music-management/src/app/dashborder/content.tsx
+++ apps/music-management/src/app/dashborder/content.tsx
@@ -16,21 +16,21 @@ export default function Content() {
           <PkmerIcon className='text-green-600' size={50} icon='iconoir:headset' />{' '}
           <div className={contentStyle.itemDescWrapper}>
             <span className='text-xl font-bold'>233</span>
-            <span>用户总数</span>
+            <span>歌曲总数</span>
           </div>
         </li>
         <li className={contentStyle.item}>
           <PkmerIcon className='text-red-500' size={50} icon='ep:mic' />{' '}
           <div className={contentStyle.itemDescWrapper}>
             <span className='text-xl font-bold'>233</span>
-            <span>用户总数</span>
+            <span>歌手数量</span>
           </div>
         </li>
         <li className={contentStyle.item}>
           <PkmerIcon className='text-yellow-600' size={50} icon='mi:document' />{' '}
           <div className={contentStyle.itemDescWrapper}>
             <span className='text-xl font-bold'>233</span>
-            <span>用户总数</span>
+            <span>歌单数量</span>
           </div>
         </li>
       </ul>
