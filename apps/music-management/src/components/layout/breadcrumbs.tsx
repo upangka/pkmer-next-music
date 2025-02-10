68acc447ef7e197adcc47936f668d6a06664a75f
pkmer <q10viking@outlook.com>
2025-02-10 09:34:00 +0800

    feat(breadcrumb): 添加面包屑

---
+++ apps/music-management/src/components/layout/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx'
+import Link from 'next/link'
+
+export type BreadCrumb = {
+  label: string
+  href: string
+  active?: boolean
+}
+
+interface Props {
+  breadCrumbs: BreadCrumb[]
+}
+
+export const BreadCrumbs: React.FC<Props> = ({ breadCrumbs }) => {
+  return (
+    <nav className='w-fit rounded-md border px-5 py-2'>
+      <ol className='flex items-center justify-start'>
+        {breadCrumbs.map((breadcrumb, index, arr) => (
+          <li key={index}>
+            <Link
+              href={breadcrumb.href}
+              className={clsx('text-gray-500', breadcrumb.active && 'font-bold text-gray-900')}
+            >
+              {breadcrumb.label}
+            </Link>
+            {index !== arr.length - 1 && <span className='mx-3'>/</span>}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  )
+}
