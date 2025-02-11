692d4c4f20579b18566ffd0a3e5eab9bca5d8cfd
pkmer <q10viking@outlook.com>
2025-02-11 17:11:46 +0800

    feat(song): 完成song列表的渲染

--- apps/music-management/src/app/dashboard/(content)/singer/manage/_components/mg-table.tsx
+++ apps/music-management/src/app/dashboard/(content)/singer/manage/_components/mg-table.tsx
@@ -5,7 +5,10 @@ import {
   TableBody,
   TableCell
 } from '@pkmer-music/management/components/ui/table'
+import { Avatar, AvatarFallback, AvatarImage } from '@pkmer-music/management/components/ui/avatar'
+
 import { pageQuerySong } from '@pkmer-music/management/actions'
+import tableStyle from '@pkmer-music/management/styles/table.module.scss'
 interface MgTableProps {
   pageNo: number
   pageSize: number
@@ -37,7 +40,48 @@ export const MgTable: React.FC<MgTableProps> = async ({ pageNo, pageSize, query,
             ))}
           </TableRow>
         </TableHeader>
-        <TableBody></TableBody>
+        <TableBody>
+          {data.list.length > 0 ? (
+            data.list.map(song => (
+              <TableRow key={song.id} className='hover:bg-gray-50'>
+                <TableCell className={tableStyle.tableItem}>{song.id}</TableCell>
+                <TableCell className={tableStyle.tableItem}>
+                  {/* Avatar,AvatarFallback,AvatarImage */}
+                  <Avatar>
+                    <AvatarImage src={song.pic} alt={song.name} />
+                    <AvatarFallback>{song.name}</AvatarFallback>
+                  </Avatar>
+                </TableCell>
+                <TableCell className={tableStyle.tableItem}>{song.name}</TableCell>
+                <TableCell className={tableStyle.tableItem}>{song.introduction}</TableCell>
+                <TableCell className={tableStyle.tableItem}>
+                  <div className='flex flex-col items-center justify-center gap-2'>
+                    <button>更新图片</button>
+                    <button>更新歌曲</button>
+                  </div>
+                </TableCell>
+                <TableCell className={tableStyle.tableItem}>
+                  <button>评论</button>
+                </TableCell>
+                <TableCell className={tableStyle.tableItem}>
+                  <div className='flex flex-col items-center justify-center gap-2'>
+                    <button>编辑</button>
+                    <button>删除</button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={headers.length}
+                className='border border-gray-300 py-20 text-center text-2xl text-gray-500'
+              >
+                Empty
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
       </Table>
     </>
   )
