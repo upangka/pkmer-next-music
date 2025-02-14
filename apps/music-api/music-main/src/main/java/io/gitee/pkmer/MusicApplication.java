1efd799ede05fb6bc129ac63722f83cfc74876fb
pkmer <q10viking@outlook.com>
2025-02-14 15:21:45 +0800

    feat(minio): minio服务已经准备好了

--- apps/music-api/music-main/src/main/java/io/gitee/pkmer/MusicApplication.java
+++ apps/music-api/music-main/src/main/java/io/gitee/pkmer/MusicApplication.java
@@ -1,19 +1,26 @@
 package io.gitee.pkmer;
 
 
+import io.gitee.pkmer.minio.repository.mybatis.FileMetadataInfoMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author <a href="mailto:1193094618@qq.com">pkmer</a>
  * <br>
  * At 2024/12/31
  */
+@Slf4j
 @SpringBootApplication
-public class MusicApplication {
+public class MusicApplication{
 
     public static void main(String[] args) {
-        SpringApplication.run(MusicApplication.class, args);
+        SpringApplication app = new SpringApplication(MusicApplication.class);
+        app.run(args);
     }
 
 }
