28589823fb664de3b068727bcb1e1525d57e1776
pkmer <q10viking@outlook.com>
2025-02-16 15:03:44 +0800

    feat(组件位置): 分析组件state没有被摧毁的问题

--- apps/music-management/src/app/test/dialog/_components/test-input.tsx
+++ apps/music-management/src/app/test/dialog/_components/test-input.tsx
@@ -10,24 +10,26 @@ interface Props {
 const TestInput: React.FC<Props> = ({ open, action }) => {
   const [msg, setMsg] = useState('')
 
+  //   方式1
   const style: React.CSSProperties = {
     display: open ? 'block' : 'none'
   }
 
   useEffect(() => {
-    console.log('TestInput mount')
+    console.log('TestInput 挂载')
     return () => {
-      console.log('TestInput unmount')
+      console.log('TestInput 卸载')
     }
   }, [])
 
   return (
     <section
+      // 方式1
       style={style}
-      className='flex w-1/4 flex-col gap-3 rounded-md border border-black p-5 shadow-lg'
+      className='flex h-fit w-fit flex-col items-center justify-start gap-3 rounded-md border border-black p-5 shadow-lg'
     >
       {open && <p className='text-center text-lg font-bold text-green-400'>内容:{msg}</p>}
-      <input className='border border-black' onChange={e => setMsg(e.target.value)} />
+      <input className='border border-black' value={msg} onChange={e => setMsg(e.target.value)} />
 
       <button
         className='rounded-md border border-black px-2 py-1 hover:bg-blue-600 hover:text-white'
--- apps/music-management/src/app/test/dialog/page.tsx
+++ apps/music-management/src/app/test/dialog/page.tsx
@@ -4,9 +4,11 @@ import TestInput from './_components/test-input'
 export default function Page() {
   const [open, setOpen] = useState(false)
   return (
-    <section className='flex flex-col items-center justify-center gap-4'>
-      {/* {open && <TestInput open={open} action={() => setOpen(false)} />} */}
+    <section className='flex w-fit flex-col items-center justify-center gap-4 p-2'>
+      修复之前：组件始终在同一个位置，不会因为传递属性的不同就会被React视为不同的组件
       <TestInput open={open} action={() => setOpen(false)} />
+      {/* 修复之后：确保组件在UI Tree中处于不同的位置 */}
+      {open && <TestInput open={open} action={() => setOpen(false)} />}
       <button
         className='rounded-md border border-black px-2 py-1 hover:bg-blue-600 hover:text-white'
         onClick={() => setOpen(true)}
