25148f77da0a88d926369914c7117d5d51c5d715
pkmer <q10viking@outlook.com>
2025-02-18 22:12:13 +0800

    feat(radio): 增加radio，性别选项

--- apps/music-management/src/app/dashboard/(content)/singer/_components/add-singer.tsx
+++ apps/music-management/src/app/dashboard/(content)/singer/_components/add-singer.tsx
@@ -15,11 +15,15 @@ import {
   PopoverTrigger
 } from '@pkmer-music/management/components/ui/popover'
 import { Input } from '@pkmer-music/management/components/ui/input'
+import { Label } from '@pkmer-music/management/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@pkmer-music/management/components/ui/radio-group'
+
 import { PkmerDialog } from './pkmer-dialog'
 import { SexEnum, type SingerBase } from '@pkmer-music/management/types'
+import { AREA } from '@pkmer/libs/constants'
+
 interface AddSingerProps {}
 export const AddSinger: React.FC<AddSingerProps> = props => {
-  const [date, setDate] = useState<Date>()
   const [singer, setSinger] = useImmer<SingerBase>({
     name: '',
     sex: SexEnum.UNKNOWN,
@@ -30,12 +34,46 @@ export const AddSinger: React.FC<AddSingerProps> = props => {
   })
   return (
     <PkmerDialog title='添加歌手'>
-      <PkmerForm>
+      <PkmerForm className='gap-7'>
         {/* 姓名start */}
         <PkmerFormItem label='姓名'>
-          <Input placeholder='请输入姓名' />
+          <Input
+            placeholder='请输入姓名'
+            value={singer.name}
+            onChange={e =>
+              setSinger(draft => {
+                draft.name = e.target.value
+              })
+            }
+          />
         </PkmerFormItem>
         {/* 姓名end */}
+        {/* 性别start */}
+        <PkmerFormItem label='性别'>
+          <RadioGroup
+            onValueChange={value =>
+              setSinger(draft => {
+                draft.sex = +value as SexEnum
+              })
+            }
+            defaultValue={(singer.sex ?? SexEnum.UNKNOWN).toString()}
+            className='flex flex-row gap-5'
+          >
+            <div className='flex items-center space-x-2'>
+              <RadioGroupItem value={SexEnum.FEMALE.toString()} id='female' />
+              <Label htmlFor='female'>女</Label>
+            </div>
+            <div className='flex items-center space-x-2'>
+              <RadioGroupItem value={SexEnum.MALE.toString()} id='male' />
+              <Label htmlFor='male'>男</Label>
+            </div>
+            <div className='flex items-center space-x-2'>
+              <RadioGroupItem value={SexEnum.UNKNOWN.toString()} id='unknown' />
+              <Label htmlFor='unknown'>未知</Label>
+            </div>
+          </RadioGroup>
+        </PkmerFormItem>
+        {/* 性别end */}
         {/* 生日start */}
         <PkmerFormItem label='生日'>
           <Popover>
@@ -43,16 +81,28 @@ export const AddSinger: React.FC<AddSingerProps> = props => {
               <Button
                 variant={'outline'}
                 className={cn(
-                  'w-[280px] justify-start text-left font-normal',
-                  !date && 'text-muted-foreground'
+                  'w-full justify-start text-left font-normal',
+                  !singer.birth && 'text-muted-foreground'
                 )}
               >
                 <CalendarIcon className='mr-2 h-4 w-4' />
-                {date ? format(date, 'yyyy-MM-dd') : <span>Pick a date</span>}
+                {singer.birth ? singer.birth : <span>Pick a date</span>}
               </Button>
             </PopoverTrigger>
             <PopoverContent className='w-auto p-0'>
-              <Calendar mode='single' selected={date} onSelect={setDate} initialFocus required />
+              <Calendar
+                mode='single'
+                selected={singer.birth ? new Date(singer.birth) : undefined}
+                onSelect={date => {
+                  if (date) {
+                    setSinger(draft => {
+                      draft.birth = format(date, 'yyyy-MM-dd')
+                    })
+                  }
+                }}
+                initialFocus
+                required
+              />
             </PopoverContent>
           </Popover>
         </PkmerFormItem>
